b5c1de257e614d25ab570692ccc1bbb9b8b4bec8
epsilon <martin.olivier1997@gmail.com>
2024-07-27 19:15:41 +0200

    fixed convergence

--- main.py
+++ main.py
@@ -17,31 +17,32 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Generate random seed points lieing inside the input image using rejection sampling
 seed_pts = np.zeros((args.num_pts, 2)) 
 for i in range(args.num_pts):
-    for j in range(50):
+    for j in range(500):
         x = np.random.randint(img_gray.shape[0] - 1)
         y = np.random.randint(img_gray.shape[1] - 1)
-        seed_pts[i] = np.array([x, y])
+        seed_pts[i] = np.array([x * 1.0/img_gray.shape[0], y * 1.0/img_gray.shape[1]])
         if np.random.uniform() < 1 - img_gray[x, y]/255.:
             break
 
 #Generate a Voronoi Diagram
-x_pts = [i for i in range(img_gray.shape[0])] 
-y_pts = [i for i in range(img_gray.shape[1])] 
+x_pts = [i * 1.0/img_gray.shape[0] for i in range(img_gray.shape[0])] 
+y_pts = [i * 1.0/img_gray.shape[1] for i in range(img_gray.shape[1])] 
 pts = [[x, y] for x in x_pts for y in y_pts]
 for n in range(args.num_iter):
+    print(n, seed_pts)
     kd = KDTree(seed_pts)
     out_img = np.zeros((img_gray.shape[0], img_gray.shape[1], 3))
     centroids = np.zeros((args.num_pts, 3))
     _, idx_list = kd.query(pts, workers = 4)
-    for idx in idx_list:
-        i, j = int(seed_pts[idx][0]), int(seed_pts[idx][1])
-        if 0 < i < img_gray.shape[0] and 0 < j < img_gray.shape[1]:
-            centroids[idx][0] += i * (1 - img_gray[i , j]/255.)
-            centroids[idx][1] += j * (1- img_gray[i, j]/255.)
-            centroids[idx][2] += 1 - img_gray[i, j]/255.
+    for pt, idx in zip(pts, idx_list):
+        x, y = pt[0], pt[1]
+        i, j = int(x * img_gray.shape[0]), int(y * img_gray.shape[1])
 
-    out = []
+        centroids[idx][0] += x * (1 - img_gray[i , j]/255.)
+        centroids[idx][1] += y * (1- img_gray[i, j]/255.)
+        centroids[idx][2] += 1 - img_gray[i, j]/255.
     w = np.power(n + 1, -0.8) * args.learning_rate 
+    i = 0 
     for sp, cen in zip(seed_pts, centroids):
         if cen[2] != 0:
             x = cen[0]/ cen[2]
@@ -50,11 +51,15 @@ for n in range(args.num_iter):
             x = sp[0]
             y = sp[1]
         
-        sp[0] += (sp[0] - x) * np.random.uniform(-1, 1) * w 
-        sp[1] += (sp[1] - y) * np.random.uniform(-1, 1) * w
+        seed_pts[i][0] -= (sp[0] - x) * w 
+        seed_pts[i][1] -= (sp[1] - y) * w
 
-        if 0 < sp[0] < img_gray.shape[0] and 0 < sp[1] < img_gray.shape[1]:
-            out_img[int(sp[0])][int(sp[1]), :] = [255, 255, 255] 
-
+        if 0 < x < 1 and 0 < y < 1:
+        #    out_img[int(x)][int(y), :] = [255, 255, 255] 
+            out_img[int(x * img_gray.shape[0])][int(y * img_gray.shape[1]), :] = [0, 0, 255] 
+        if 0 < sp[0] < 1 and 0 < sp[1] < 1:
+            out_img[int(sp[0] * img_gray.shape[0])][int(sp[1] * img_gray.shape[1]), :] = [255, 255, 255] 
+        i += 1
+    print(seed_pts)
     output_filename = str(args.input_filename.with_stem(f"{args.input_filename.stem}_stippled_{n}"))
     cv2.imwrite(f"out/{output_filename}", out_img)
