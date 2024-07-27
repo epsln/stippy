15ffb9acb4f7c9dae94042b057f99a8ca08f7feb
epsilon <martin.olivier1997@gmail.com>
2024-07-27 17:48:06 +0200

    added bounds checks and transformed space

--- main.py
+++ main.py
@@ -20,25 +20,25 @@ for i in range(args.num_pts):
     for j in range(50):
         x = np.random.randint(img_gray.shape[0] - 1)
         y = np.random.randint(img_gray.shape[1] - 1)
-        seed_pts[i] = np.array([x * 1.0/img_gray.shape[0], y * 1.0/img_gray.shape[1]])
+        seed_pts[i] = np.array([x, y])
         if np.random.uniform() < 1 - img_gray[x, y]/255.:
             break
 
 #Generate a Voronoi Diagram
-x_pts = np.linspace(0, 1, img_gray.shape[0] - 1)
-y_pts = np.linspace(0, 1, img_gray.shape[1] - 1)
+x_pts = [i for i in range(img_gray.shape[0])] 
+y_pts = [i for i in range(img_gray.shape[1])] 
 pts = [[x, y] for x in x_pts for y in y_pts]
 for n in range(args.num_iter):
     kd = KDTree(seed_pts)
     out_img = np.zeros((img_gray.shape[0], img_gray.shape[1], 3))
     centroids = np.zeros((args.num_pts, 3))
-    _, idx_list = kd.query(pts, workers = 2)
+    _, idx_list = kd.query(pts, workers = 4)
     for idx in idx_list:
-        x, y = seed_pts[idx][0], seed_pts[idx][1]
-        i, j = int(x * img_gray.shape[0]), int(y * img_gray.shape[1])
-        centroids[idx][0] += x * (1 - img_gray[i , j]/255.)
-        centroids[idx][1] += y * (1- img_gray[i, j]/255.)
-        centroids[idx][2] += 1 - img_gray[i, j]/255.
+        i, j = int(seed_pts[idx][0]), int(seed_pts[idx][1])
+        if 0 < i < img_gray.shape[0] and 0 < j < img_gray.shape[1]:
+            centroids[idx][0] += i * (1 - img_gray[i , j]/255.)
+            centroids[idx][1] += j * (1- img_gray[i, j]/255.)
+            centroids[idx][2] += 1 - img_gray[i, j]/255.
 
     out = []
     w = np.power(n + 1, -0.8) * args.learning_rate 
@@ -53,8 +53,8 @@ for n in range(args.num_iter):
         sp[0] += (sp[0] - x) * np.random.uniform(-1, 1) * w 
         sp[1] += (sp[1] - y) * np.random.uniform(-1, 1) * w
 
-        if sp[0] > 0 and sp[0] < 1 and sp[1] > 0 and sp[1] < 1:
-            out_img[int(sp[0] * img_gray.shape[0])][int(sp[1] * img_gray.shape[1]), :] = [255, 255, 255] 
+        if 0 < sp[0] < img_gray.shape[0] and 0 < sp[1] < img_gray.shape[1]:
+            out_img[int(sp[0])][int(sp[1]), :] = [255, 255, 255] 
 
     output_filename = str(args.input_filename.with_stem(f"{args.input_filename.stem}_stippled_{n}"))
     cv2.imwrite(f"out/{output_filename}", out_img)
